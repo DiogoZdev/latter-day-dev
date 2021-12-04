cd6d4143cfe3798eb22d6c998221188c807c4aa1
DiogoZ <diogozang2@gmail.com>
2021-12-04 13:44:00 -0300

    test adjusted

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('latter-day-dev app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('ldd');
   });
 });
